009a586da83ea241fba995f0bbd5ef1a4dcad01f
ahah525 <ahah525@naver.com>
2022-09-22 13:39:21 +0900

    feat : 게시글 작성 기능 구현(오류 해결)

--- src/main/java/com/ll/exam/app_2022_09_23/app/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/exam/app_2022_09_23/app/article/repository/ArticleRepository.java
@@ -1,13 +1,16 @@
 package com.ll.exam.app_2022_09_23.app.article.repository;
 
 import com.ll.exam.app_2022_09_23.app.article.dto.Article;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 @Mapper
 public interface ArticleRepository {
+
     @Select("""
             <script>
             SELECT *
@@ -15,4 +18,12 @@ public interface ArticleRepository {
             </script>
             """)
     List<Article> getArticles();
+
+    @Insert("""
+            <script>
+            INSERT INTO article(createDate, modifyDate, subject, content)
+            values(NOW(), NOW(), #{subject}, #{content}) 
+            </script>
+            """)
+    void write(@Param("subject") String subject, @Param("content") String content);
 }
